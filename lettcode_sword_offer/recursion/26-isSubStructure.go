b5acf1b35cf840f68a6d22da54cf84e912305d7d
xvpenghao <xupenghao1996@163.com>
2022-06-17 10:29:03 +0800

    剑指 Offer 26. 树的子结构

---
+++ lettcode_sword_offer/recursion/26-isSubStructure.go
@@ -0,0 +1,91 @@
+package recursion
+
+// @Author XuPEngHao
+// @DATE 2022/6/1 09:12
+
+/*
+剑指 Offer 26. 树的子结构
+输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+
+B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+
+例如:
+给定的树 A:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 B：
+
+   4
+  /
+ 1
+返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+
+示例 1：
+
+输入：A = [1,2,3], B = [3,1]
+输出：false
+示例 2：
+
+输入：A = [3,4,5,1,2], B = [4,1]
+输出：true
+限制：
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 节点的左右页需要对上。
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	if B == nil {
+		return false
+	}
+	// 找到符号的子树吗，知道没有更合适的节点
+	A = findNodeByTarget(A, B.Val)
+	if A == nil {
+		return false
+	}
+	if dfsEqual(A, B) {
+		return true
+	}
+	return isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
+}
+
+func dfsEqual(A *TreeNode, B *TreeNode) bool {
+	// 不相等立马退出
+	if A != nil && B != nil && A.Val != B.Val {
+		return false
+	}
+	if A == nil && B != nil {
+		return false
+	}
+	if A != nil && B != nil {
+		return dfsEqual(A.Left, B.Left) && dfsEqual(A.Right, B.Right)
+	}
+	return true
+}
+
+// 找打要比较的节点位置
+func findNodeByTarget(A *TreeNode, targetNum int) *TreeNode {
+	if A == nil {
+		return nil
+	}
+	if A.Val == targetNum {
+		return A
+	}
+	if node := findNodeByTarget(A.Left, targetNum); node != nil {
+		return node
+	}
+	if node := findNodeByTarget(A.Right, targetNum); node != nil {
+		return node
+	}
+	return nil
+}
---
+++ lettcode_sword_offer/recursion/recursion_test.go
@@ -0,0 +1,53 @@
+package recursion
+
+import "testing"
+
+// @Author XuPEngHao
+// @DATE 2022/6/1 10:05
+
+func Test_isSubStructure(t *testing.T) {
+	A := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 0,
+			Left: &TreeNode{
+				Val: -4,
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 1,
+		},
+	}
+	B := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: -4,
+		},
+	}
+	//
+	// A = &TreeNode{
+	// 	Val: 3,
+	// 	Left: &TreeNode{
+	// 		Val: 4,
+	// 		Left: &TreeNode{
+	// 			Val: 1,
+	// 		},
+	// 		Right: &TreeNode{
+	// 			Val: 2,
+	// 		},
+	// 	},
+	// 	Right: &TreeNode{
+	// 		Val: 5,
+	// 	},
+	// }
+	// B = &TreeNode{
+	// 	Val: 4,
+	// 	Left: &TreeNode{
+	// 		Val: 1,
+	// 	},
+	// }
+	t.Log(isSubStructure(A, B))
+}
